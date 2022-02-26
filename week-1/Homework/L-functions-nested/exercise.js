2363cdb837618a30a7bae9cd225f80984260eec0
gabiarno <gabiarno@hotmail.com>
2022-02-26 15:48:10 +0100

    homework

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -5,12 +5,18 @@ var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
 
-function greetings(mentor1,mentor2,mentor3,mentor4,mentor5){
-    console.log("HELLO " + mentor1.toUpperCase());
-    console.log("HELLO " + mentor2.toUpperCase());
-    console.log("HELLO " + mentor3.toUpperCase());
-    console.log("HELLO " + mentor4.toUpperCase());
-    console.log("HELLO " + mentor5.toUpperCase());
+
+function nameInUpperCase(name){
+    return (name.toUpperCase());
 }
 
-greetings(mentor1,mentor2,mentor3,mentor4,mentor5)
+function writeGreeting(name){
+    var greeting = "HELLO " + nameInUpperCase(name);
+    console.log(greeting);
+}
+
+writeGreeting(mentor1)
+writeGreeting(mentor2)
+writeGreeting(mentor3)
+writeGreeting(mentor4)
+writeGreeting(mentor5)
